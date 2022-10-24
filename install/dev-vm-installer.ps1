7d12b2043563a1329427a3f6e044116d0f36d33c
gfzyriek <71277637+gfzyriek@users.noreply.github.com>
2022-10-24 17:07:51 -0500

    Add files via upload

---
+++ install/dev-vm-installer.ps1
@@ -0,0 +1,12 @@
+choco install insomnia-rest-api-client --version 2022.5.0 -y
+choco install python --version 3.10.8 -y
+choco install sql-server-2019 --version 15.0.2000.20210324 -y
+choco install git-fork --version 1.78.1 -y
+choco install nodejs-lts --version 16.18.0 -y
+choco install yarn --version 1.22.19 -y
+choco install microsoftazurestorageexplorer --version 1.25.1 -y
+choco install cmder --version 1.3.20 -y
+choco install resharper --version 2022.2.3 -y
+choco install agentransack --version 2022.3326 -y
+choco install azure-data-studio --version 1.39.1 -y
+choco install linqpad --version 7.4.9 -y
